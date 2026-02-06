c50586f5a85a5947555c70a2b7e57d9fd431f80d
dalerjon555 <dilovarbotirova0@gmail.com>
2026-02-06 18:57:06 +0500

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,32 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
 
-/*=============== SHOW SCROLL UP ===============*/ 
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_5aehycs', 'template_iwu9qpn', 'contact-form', 'CUVuD8UdNB1epLP7T')
+
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅'
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            // Clear input fields
+            contactForm.reset()
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+        })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
